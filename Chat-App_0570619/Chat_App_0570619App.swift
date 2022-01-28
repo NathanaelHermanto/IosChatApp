ea535e2f902c74280196b88191bbb44f41138818
Nathanael Hermanto <nathanael887@gmail.com>
2022-01-28 13:48:48 +0100

    Initial Commit

---
+++ Chat-App_0570619/Chat_App_0570619App.swift
@@ -0,0 +1,17 @@
+//
+//  Chat_App_0570619App.swift
+//  Chat-App_0570619
+//
+//  Created by Nathan on 28.01.22.
+//
+
+import SwiftUI
+
+@main
+struct Chat_App_0570619App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chat-App_0570619/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Chat-App_0570619
+//
+//  Created by Nathan on 28.01.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
